e6e5469a074349169ccecdd76698063edd377d02
Emre Tatlici <m.emretatlici@icloud.com>
2023-12-08 17:39:54 +0300

    Initial commit

---
+++ healthMetrics-data-analyzing.sql
@@ -0,0 +1,24 @@
+-- Examine Data Types
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'HealthMetrics';
+
+-- Identify Unique Values
+SELECT DISTINCT locationAbbr, locationDesc, age_years
+FROM HealthMetrics;
+
+-- Replace Not Available Values with NULL
+UPDATE HealthMetrics
+SET age_years = NULL
+WHERE age_years = 'Not Available';
+
+-- Analyze the distribution of data value across different age groups.
+SELECT
+  age_years,
+  COUNT(*) AS Count,
+  AVG(data_value) AS MeanDataValue
+FROM HealthMetrics
+WHERE data_value IS NOT NULL
+GROUP BY age_years;
+
+select * From HealthMetrics
---
+++ healthMetrics-data-cleaning.sql
@@ -0,0 +1,63 @@
+SELECT *
+FROM HealthMetrics
+WHERE Data_Value_Footnote IS NULL;
+
+
+-- Check for missing values in the entire dataset
+SELECT COUNT(*) AS NullCount
+FROM HealthMetrics
+WHERE Data_Value_Footnote IS NULL;
+
+-- Deleting Columns That has too many missing values or unnecessary
+ALTER TABLE HealthMetrics
+DROP COLUMN Low_Confidence_Limit,High_Confidence_Limit,Data_Value_Alt,YearEnd,Topic,Class,DataSource,QuestionID,ClassID,TopicID,
+DataValueTypeID,Data_Value_Type,StratificationCategoryId1,StratificationID1,Data_Value_Unit, Data_Value_Footnote_Symbol, Data_Value_Footnote, Total;
+
+ALTER TABLE HealthMetrics
+DROP COLUMN GeoLocation, Question, StratificationCategory1, Stratification1, Gender
+
+
+-- Replace null values with a default value (adjust as needed) / Age_years, Education, Income, Race_Ethnicity
+UPDATE HealthMetrics
+SET Income = 'Not Available'
+WHERE Income IS NULL;
+
+
+-- Rename column from 'Education' to 'education' as well as other columns
+EXEC sp_rename 'HealthMetrics.Education', 'education', 'COLUMN';
+
+
+-- Get the data type of columns
+SELECT DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'HealthMetrics'
+
+
+
+
+-- Calculate percentage of 'Not Available' values for all columns in the 'HealthMetrics' table
+DECLARE @tableName NVARCHAR(255) = 'HealthMetrics';
+DECLARE @sql NVARCHAR(MAX) = '';
+
+SELECT @sql += 
+    'SELECT ''' + COLUMN_NAME + ''' AS ColumnName, ' +
+    'COUNT(*) AS TotalRows, ' +
+    'SUM(CASE WHEN ' + COLUMN_NAME + ' = ''Not Available'' THEN 1 ELSE 0 END) AS NotAvailableCount, ' +
+    'CAST(SUM(CASE WHEN ' + COLUMN_NAME + ' = ''Not Available'' THEN 1 ELSE 0 END) AS FLOAT) / NULLIF(CAST(COUNT(*) AS FLOAT), 0) * 100 AS NotAvailablePercentage ' +
+    'FROM ' + @tableName + ' UNION ALL '
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @tableName
+AND DATA_TYPE IN ('VARCHAR', 'NVARCHAR', 'CHAR', 'NCHAR'); -- Add more data types if needed
+
+-- Remove the last 'UNION ALL'
+SET @sql = LEFT(@sql, LEN(@sql) - LEN('UNION ALL'));
+
+-- Execute the generated SQL
+EXEC sp_executesql @sql;
+
+-- Drop columns with a high percentage of 'Not Available' values
+ALTER TABLE HealthMetrics
+DROP COLUMN education, income, race_ethnicity;
+
+
+
